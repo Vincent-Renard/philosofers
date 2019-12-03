faa9f040a8427a02560c349e1ba67ffc13ed7b3e
Vincent <vincent.renard@gmx.com>
2019-12-03 15:42:27 +0100

    gen_automate + systemes

--- trucs/gen_automate.py
+++ trucs/gen_automate.py
@@ -7,12 +7,14 @@ import itertools
 if __name__ == '__main__':
 
     nbr_philos = 4  # argv[1]
-
+    sys_philo="P = [T,H,E] {\netat = 3;\ninit = 0;\n0=T;\n1=H;\n2=E;\n0 -> 1 [goH];\n1 -> 2 [goE];\n2 -> 0 [goT];\n0 -> 0 [remainT];\n1 -> 1 [remainHE];\n2 -> 2 [remainHE];\n};;\n"
+    sys_canal="C = [N,L,R] {\netat = 3;\ninit = 0;\n0=N;\n1=L;\n2=R;\n0 -> 1 [addL];\n0 -> 2 [addR];\n0 -> 0 [delL];\n1 -> 0 [delL];\n0 -> 0 [delR];\n2 -> 0 [delR];\n0 -> 0 [remainNL];\n1 -> 1 [remainNL];\n0 -> 0 [remainNR];\n2 -> 2 [remainNR];\n};;\n"
     state_HUNGRY = 'HUNGRY'
     state_THINK = 'THINK'
     state_EAT = 'EAT'
-
-    s = "systeme = <"
+    s=""
+    s+=sys_philo+sys_canal
+    s += "systeme = <"
     s += ",".join(["P p" + str(i) for i in range(nbr_philos)]) + "," + ",".join(
         ["C c" + str(i) for i in range(nbr_philos)]) + "> {\n"
 
@@ -39,5 +41,9 @@ if __name__ == '__main__':
         s += "<" + ",".join(["goT" if j == i else "_" for j in range(nbr_philos)])
         s += "," + ",".join(["delL" if (i - 1) % nbr_philos == j else "delR" if i == j else "_" for j in range(nbr_philos)]) + ">;\n"
 
-    s += "};;"
+    s += "};;\n"
+
+    s+="todot verif_test.dot systeme;;\n"
+
+
     print(s)
