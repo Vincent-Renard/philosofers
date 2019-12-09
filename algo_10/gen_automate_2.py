50f63b1d8fcf9b0081f062710acf102ab86babc2
Vincent <vincent.renard@gmx.com>
2019-12-09 23:47:52 +0100

    usage args

--- algo_10/gen_automate_2.py
+++ algo_10/gen_automate_2.py
@@ -3,27 +3,63 @@
 from sys import argv
 import itertools
 if __name__ == '__main__':
+    if len(argv)<2:
+        print("usage : ./gen_automate_XX.py n_philosophes [n_canaux (si non fourni -> n_canaux = nbr_philos)]")
     nbr_philos = int(argv[1]) #TODO metre mode d'emploi si l'argument n'est pas fourni
     nbr_canaux = nbr_philos-1
-    sys_philo="P = [T,H,E] {\netat = 3;\ninit = 0;\n0=T;\n1=H;\n2=E;\n0 -> 1 [goH];\n1 -> 2 [goE];\n2 -> 0 [goT];\n0 -> 0 [remainT];\n1 -> 1 [remainHE];\n2 -> 2 [remainHE];\n};;\n"
-    sys_canal="C = [N,L,R] {\netat = 3;\ninit = 0;\n0=N;\n1=L;\n2=R;\n0 -> 1 [addL];\n0 -> 2 [addR];\n0 -> 0 [delL];\n1 -> 0 [delL];\n0 -> 0 [delR];\n2 -> 0 [delR];\n0 -> 0 [remainNL];\n1 -> 1 [remainNL];\n0 -> 0 [remainNR];\n2 -> 2 [remainNR];\n};;\n"
-    state_HUNGRY = 'HUNGRY'
-    state_THINK = 'THINK'
-    state_EAT = 'EAT'
+
+    if len(argv)>2:
+        nbr_canaux =int(argv[2])
+
+
+    sys_philo=
+"""
+P = [T,,E] {
+etat = 3;
+init = 0;
+0=T;
+1=H;
+2=E;
+0 -> 1 [T];
+1 -> 2 [H];
+2 -> 3 [E];
+3 -> 0 [T];
+};;
+"""
+    sys_canal=
+"""
+C = [P,V] {
+etat = 2;
+init = 0;
+0=P;
+1=V;
+0 -> 1 [V];
+1 -> 0 [P];
+};;
+"""
+"""
+
+<T,H,E,P,V,P>
+
+
+"""
+
     s=""
     s+=sys_philo+sys_canal
     s += "systeme = <"
-    s += ",".join(["P p" + str(i) for i in range(nbr_philos)]) + "," + ",".join(
-        ["C c" + str(i) for i in range(nbr_canaux)]) + "> {\n"
+    s += ",".join(["P p" + str(i) for i in range(nbr_philos)]) + "," + ",".join(["C c" + str(i) for i in range(nbr_canaux)]) + "> {\n"
     for philo in range(nbr_philos):
-        s += "<" + ",".join(["goH" if i == philo else "remainT" if (i - 1) == philo or (i + 1) == philo else "_" for i in range(nbr_philos)])
+        s += "<" + ",".join(["H" if i == philo else "_" for i in range(nbr_philos)])
+        s += "," + ",".join(["V" if i == philo else "_" for i in range(nbr_canaux)])
+
+        s += "<" + ",".join(["E" if i == philo else "_" if (i - 1) == philo or (i + 1) == philo else "_" for i in range(nbr_philos)])
         s += "," + ",".join(["_" for i in range(nbr_canaux)]) + ">;\n"
-        s += "<" + ",".join(["goH" if i == philo else "remainHE" if (i - 1) == philo else "remainT" if (i + 1) == philo else "_" for i in range(nbr_philos)])
-        s += "," + ",".join(["addL" if i == philo else "_" for i in range(nbr_canaux)]) + ">;\n"
-        s += "<" + ",".join(["goH" if i == philo else "remainT" if (i - 1) == philo else "remainHE" if (i + 1) == philo else "_" for i in range(nbr_philos)])
+        s += "<" + ",".join(["E" if i == philo else "remainHE" if (i - 1) == philo else "_" if (i + 1) == philo else "_" for i in range(nbr_philos)])
+        s += "," + ",".join(["T" if i == philo else "_" for i in range(nbr_canaux)]) + ">;\n"
+        s += "<" + ",".join(["E" if i == philo else "_" if (i - 1) == philo else "remainHE" if (i + 1) == philo else "_" for i in range(nbr_philos)])
         s += "," + ",".join(["addR" if (i + 1) == philo else "_" for i in range(nbr_canaux)]) + ">;\n"
-        s += "<" + ",".join(["goH" if i == philo else "remainHE" if (i - 1) == philo or (i + 1) == philo else "_" for i in range(nbr_philos)])
-        s += "," + ",".join(["addL" if i == philo else "addR" if (i + 1) == philo else "_" for i in range(nbr_canaux)]) + ">;\n"
+        s += "<" + ",".join(["E" if i == philo else "remainHE" if (i - 1) == philo or (i + 1) == philo else "_" for i in range(nbr_philos)])
+        s += "," + ",".join(["T" if i == philo else "addR" if (i + 1) == philo else "_" for i in range(nbr_canaux)]) + ">;\n"
         s += "\n"
     for i in range(nbr_philos):
         s += "<" + ",".join(["goE" if j == i else "_" for j in range(nbr_philos)])
